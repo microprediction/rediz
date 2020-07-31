fa3439f3a34be64ec2bbe48f753a38e510d4e21d
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-07-31 10:03:08 -0400

    Delete rediz_test_config_private.py

--- rediz/rediz_test_config_private.py
+++
@@ -1,6 +0,0 @@
-import os
-
-NOTHING_MUCH = 'NOTHING_MUCH'
-os.environ['TEST_CONFIG_URL']='https://config.microprediction.org/t3stc0nf1g.json'
-os.environ['TEST_CONFIG_FAILOVER_URL']='https://stableconfig.microprediction.org/t3stc0nf1g.json'
-
