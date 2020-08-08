01e5688d8c55e99e8889a08193659c39d564db59
Peter Cotton <pcotton@intechinvestments.com>
2020-08-07 22:03:06 -0400

    version bump after fix to config urls in github

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="rediz",
-    version="0.11.2",
+    version="0.11.3",
     description="Powering community nowcasts at www.microprediction.org",
     long_description=README,
     long_description_content_type="text/markdown",
