1732dc1706b8c1337b5489d70f1069cabbd2fa9e
Peter Cotton <pcotton@intechinvestments.com>
2020-08-07 21:58:26 -0400

    config

--- tests/test_config_retrieval.py
+++ tests/test_config_retrieval.py
@@ -2,4 +2,4 @@ from rediz.rediz_test_config import REDIZ_TEST_CONFIG
 import json
 
 def test_retrieval():
-    assert isinstance(REDIZ_TEST_CONFIG,dict),
+    assert isinstance(REDIZ_TEST_CONFIG,dict), json.dumps(REDIZ_TEST_CONFIG)
