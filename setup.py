cf912d6a120da13e8018ffe8aab5025dd9ce15d1
Peter Cotton <peter.cotton@microprediction.com>
2020-09-26 10:27:15 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="rediz",
-    version="0.13.3",
+    version="0.2.0",
     description="Powering community nowcasts at www.microprediction.org",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest', 'microconventions', 'fakeredis'],
     include_package_data=True,
-    install_requires=["microconventions>=0.1.0", "fakeredis", "getjson", "redis", "sortedcontainers", "numpy",
+    install_requires=["microconventions==0.1.0", "fakeredis", "getjson", "redis", "sortedcontainers", "numpy",
                       "pymorton", "scipy", "pathlib"],
     entry_points={
         "console_scripts": [
