e5857dee69faf3e35a0b0b80fa722365fbb08489
Peter Cotton <peter.cotton@microprediction.com>
2021-10-29 11:21:03 -0400

    blah

--- m6/ethereum_qqplot.py
+++ m6/ethereum_qqplot.py
@@ -1,12 +1,19 @@
 from rediz.client import Rediz
 from rediz.collider_config_private import REDIZ_COLLIDER_CONFIG, EMBLOSSOM_MOTH
 from pprint import pprint
+import numpy as np
 
 if __name__ == '__main__':
+    try:
+        import statsmodels.api as sm
+    except ImportError:
+        raise Exception('pip install statsmodels')
     rdz = Rediz(**REDIZ_COLLIDER_CONFIG)
     NAME = 'c5_ethereum.json'
     samples = rdz.get_predictions(name=NAME,delay=rdz.DELAYS[-1], write_key=EMBLOSSOM_MOTH)
     pprint(samples)
     print(len(samples))
-    import statsmodels.api as sm
-    sm.qqplot(test, line='45')
+    data = np.array(list(samples.values()))
+    sm.qqplot(data, line='45',fit=True)
+    import matplotlib.pyplot as plt
+    plt.show()
