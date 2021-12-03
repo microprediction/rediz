0963d2c3953d2a8112e124bb454a6bf41d2b3583
Peter Cotton <peter.cotton@microprediction.com>
2021-12-03 09:44:54 -0500

    noise

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="rediz",
-    version="0.16.5",
+    version="0.16.6",
     description="Powering community nowcasts at www.microprediction.org",
     long_description=README,
     long_description_content_type="text/markdown",
